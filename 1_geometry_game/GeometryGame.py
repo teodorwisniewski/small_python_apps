7419d84ef27188759de45bd9911a660198595f53
Teodor <teodorwis90@gmail.com>
2021-10-30 17:19:37 +0200

    RandomPoint object definition

--- 1_geometry_game/GeometryGame.py
+++ 1_geometry_game/GeometryGame.py
@@ -4,7 +4,9 @@ import random
 class GeometryGame:
 
     def __init__(self):
-        pass
+        self.bottom_left_pnt = RandomPoint()
+        self.upper_right_pnt = RandomPoint(min_x=self.bottom_left_pnt.x,
+                                     min_y=self.bottom_left_pnt.y)
 
     def run(self):
         pass
@@ -14,6 +16,13 @@ class GeometryGame:
 
     def get_result(self):
         pass
+
+
+class RandomPoint:
+
+    def __init__(self, min_x=0, min_y=0, delta_x=10, delta_y=10):
+        self.x = random.randint(min_x, min_x+delta_x)
+        self.y = random.randint(min_y, min_y+delta_y)
 
 
 if __name__ == "__main__":
