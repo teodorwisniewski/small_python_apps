71004035a9b1542c18ebd30bb84ad3e591220200
Teodor <teodorwis90@gmail.com>
2021-10-30 17:31:25 +0200

    definition of get result method and guess method

--- 1_geometry_game/GeometryGame.py
+++ 1_geometry_game/GeometryGame.py
@@ -8,27 +8,39 @@ class GeometryGame:
         self.upper_right_pnt = RandomPoint(min_x=self.bottom_left_pnt.x,
                                      min_y=self.bottom_left_pnt.y)
 
-    def run(self):
-        pass
+    def run(self) -> bool:
+        self.guess()
+        result = self.get_result()
+
+        print(f"Your point is insede rectangle: {result}")
+        return result
 
     def guess(self):
-        pass
+        x_guess = input("Guess the point inside - x coordinate")
+        y_guess = input("Guess the point inside - y coordinate")
+        self.user_point = RandomPoint(x=x_guess, y=y_guess)
 
     def get_result(self):
-        pass
+        return self.bottom_left_pnt < self.user_point < self.upper_right_pnt
+
 
 
 class RandomPoint:
 
-    def __init__(self, min_x=0, min_y=0, delta_x=10, delta_y=10):
-        self.x = random.randint(min_x, min_x+delta_x)
-        self.y = random.randint(min_y, min_y+delta_y)
+    def __init__(self, x=None, y=None, min_x=0, min_y=0, delta_x=10, delta_y=10):
+        if (x is None) or (y is None):
+            self.x = random.randint(min_x, min_x+delta_x)
+            self.y = random.randint(min_y, min_y+delta_y)
+        else:
+            self.x = x
+            self.y = y
 
     def __gt__(self, other):
         return (self.x > other.x) and (self.y > other.y)
 
     def __lt__(self, other):
         return (self.x < other.x) and (self.y < other.y)
+
 
 if __name__ == "__main__":
     game = GeometryGame()
