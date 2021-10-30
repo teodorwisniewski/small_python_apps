0f4a8ac6b067bf005ca4c144abb838eb02348b2f
Teodor <teodorwis90@gmail.com>
2021-10-30 17:10:59 +0200

    geometry game template

---
+++ 1_geometry_game/GeometryGame.py
@@ -0,0 +1,21 @@
+import random
+
+
+class GeometryGame:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def guess(self):
+        pass
+
+    def get_result(self):
+        pass
+
+
+if __name__ == "__main__":
+    game = GeometryGame()
+    game.run()
